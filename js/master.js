b86e3ac26ddeeb2e32b38975e0b6eee5f5dcccab
Margaret Wangechi <wangechimargaret7@gmail.com>
2018-12-18 13:14:18 +0300

    modify contact file and style it

--- js/master.js
+++ js/master.js
@@ -1,5 +1,5 @@
-$(document).ready (function()){
-  $("#feedback form").submit(function(e){
-
+$(document).ready (function(){
+  $("#feedback form").submit(function(){
+alert("Thank you for your feedback. We will get back to you in the course of the week. ");
   });
 });
