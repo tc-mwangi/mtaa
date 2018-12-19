42dcdd4e19ee2d3a77f272bfc6db888d3eb49b80
Margaret Wangechi <wangechimargaret7@gmail.com>
2018-12-19 17:18:54 +0300

    finalize contact file

---
+++ js/contacts.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+  $("#feedback form").submit(function(e) {
+    $(".input-group").hide();
+    $("#messageContainer").hide()
+    $("#text").slideDown(2000);
+    e.preventDefault();
+
+  });
+
+  $("#submit").click(function() {
+
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+    var subject = document.getElementById('subject').value;
+    var message = document.getElementById('message').value;
+
+    if (email === "" || phone === "" || message === "") {
+      alert("Please capture all the details on the form");
+    } else {
+
+      var both = name + ' |' + email + ' |' + phone + ' |' + subject + ' |' + message;
+      var xx = new Blob([both], {
+        type: "text/plain;charset=utf-8"
+      });
+      alert("Your information has been saved");
+      saveAs(xx, "contact.txt");
+
+    }
+
+  });
+
+});
